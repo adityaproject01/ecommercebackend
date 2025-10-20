da224e936278b1c2ed7c8d997cceedaf46f993e9
adityaproject01 <aditya31031998@gmail.com>
2025-10-20 23:17:37 +0530

    s

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: process.env.DB_SSL_CA,
+  },
+});
